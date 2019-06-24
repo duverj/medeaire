36ffdfbf2af24253d77ba3545bcb248857ab7ca0
duverj <duverj@gmail.com>
2019-06-24 02:36:23 +0200

    Fixing fetch URL

--- src/components/airStations/AirStations.js
+++ src/components/airStations/AirStations.js
@@ -20,7 +20,7 @@ const AirStations = () => {
     const [stations, setStations] = useState({});
 
     async function fetchData() {
-        const res = await fetch("http://159.203.164.117:4000/air");
+        const res = await fetch("http://localhost:4000/air");
         res
             .json()
             .then(res => setStations(res.message))
